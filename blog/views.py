39ecf90c71371dd7edac4f6837e5d042bdb160c7
Moose <matt_salmon@hotmail.com>
2020-04-04 19:23:38 -0500

    website tutorial in progress

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    # return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    # return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
